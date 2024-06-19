1e19543ba37d8e1a3f71ca4395e23f4db43061f5
RyanGrogan416 <140850868+RyanGrogan416@users.noreply.github.com>
2024-06-19 16:22:02 +0000

    setting up the framework of the styles of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,27 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Playlist from "./components/JS_components/playlist_components/Playlist.js";
+import { useState } from "react";
+import TrackList from "./components/JS_components/playlist_components/TrackList.js";
+import EditableTitle from "./components/JS_components/playlist_components/EditableTitle.js";
 import SearchBar from "./components/JS_components/SeachBar_components/SearchBar.js";
+import SearchResults from "./components/JS_components/SeachBar_components/SearchResults.js";
+import style from "./components/CSS_components/grid.css";
 
 function App() {
+  const [title, setTitle] = useState("My Playlist");
+
   return (
-    <div
-      style={{
-        display: "flex",
-        backgroundColor: "pink",
-        width: "100%",
-        height: "100vh",
-      }}
-    >
-      <div
-        className="SearchBar-container"
-        style={{
-          top: "25px",
-          width: "50%",
-          height: "100vh",
-        }}
-      >
+    <div style={style.app}>
+      <div style={style.searchbar}>
         <SearchBar />
       </div>
-      <div
-        className="Playlist-container"
-        style={{
-          top: "25px",
-          width: "50%",
-          height: "100vh",
-        }}
-      >
-        <Playlist />
+      <div style={style.searchresults}>
+        <SearchResults />
+      </div>
+      <div>
+        <EditableTitle title={title} setTitle={setTitle} />
+      </div>
+      <div>
+        <TrackList />
       </div>
     </div>
   );
